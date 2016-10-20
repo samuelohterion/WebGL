0ab1c23aa7c1522e9534f7f93402412a98cd9204
Thomas Peschel <tpeschel@life.uni-leipzig.de>
2016-10-20 21:52:28 +0200

    minor changes

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
-function mein() {
-	var o = new Object( "obj" );
-	o.i = 1;
-	o.name = "hello world";
-	document.getElementById( "obj" ).innerHTML = o.i + o.name;
+function mein( ) {
+
+    var o = new Object( "obj" );
+
+    o.i = 1;
+    o.name = "hello world";
+    document.getElementById( "obj" ).innerHTML = o.i + o.name;
 }
